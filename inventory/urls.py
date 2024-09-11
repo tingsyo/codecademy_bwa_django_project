96e346099932038b8f272e9d4bc2f4e87a9bd798
Ting-Shuo Yo <tingshuo.yo@googlemail.com>
2024-09-11 18:15:10 +0800

    - Updated base.html - Updated inventory/home.html - Added bootstrap css and js

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,9 +3,11 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    path("logout/", views.log_out, name="logout"),
+    path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
     path('', views.HomeView.as_view(), name='home'),
-    path('ingredient/', views.HomeView.as_view(), name='ingredients'),
-    path('menuitem/', views.HomeView.as_view(), name='menu'),
-    path('purchase/', views.HomeView.as_view(), name='purchases'),
-    path('report/', views.HomeView.as_view(), name='reports'),
+    path('ingredient/', views.IngredientListView.as_view(), name='ingredients'),
+    path('menuitem/', views.MenuItemListView.as_view(), name='menu'),
+    path('purchase/', views.PurchaseListView.as_view(), name='purchases'),
+    path('report/', views.ReportView.as_view(), name='reports'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
@@ -8,7 +9,8 @@ from django.urls import reverse_lazy
 
 
 # Create your views here.
-class HomeView(TemplateView):
+#@login_required(login_url="/accounts/login/")
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "inventory/home.html"
 
     def get_context_data(self, **kwargs):
@@ -16,12 +18,17 @@ class HomeView(TemplateView):
         context["ingredients"] = Ingredient.objects.all()
         context["menu_items"] = MenuItem.objects.all()
         context["purchases"] = Purchase.objects.all()
+        context["nbar"] = "home"
         return context
 
 class IngredientListView(LoginRequiredMixin, ListView):
     model = Ingredient
     template_name = "inventory/ingredients_list.html"
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["nbar"] = "ingrdient"
+        return context
+    
 class IngredientCreateView(LoginRequiredMixin, CreateView):
     model = Ingredient
     template_name = "inventory/ingredient_create.html"
@@ -38,7 +45,11 @@ class IngredientDeleteView(LoginRequiredMixin, DeleteView):
 class MenuItemListView(LoginRequiredMixin, ListView):
     model = MenuItem
     template_name = "inventory/menuitems_list.html"
-
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["nbar"] = "menuitem"
+        return context
+    
 class MenuItemCreateView(LoginRequiredMixin, CreateView):
     model = MenuItem
     template_name = "inventory/menuitem_create.html"
@@ -54,6 +65,10 @@ class MenuItemDeleteView(LoginRequiredMixin, DeleteView):
 class PurchaseListView(LoginRequiredMixin, ListView):
     model = Purchase
     template_name = "inventory/purchases_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["nbar"] = "purchase"
+        return context
 
 class PurchaseCreateView(LoginRequiredMixin, CreateView):
     model = Purchase
@@ -68,8 +83,17 @@ class PurchaseDeleteView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('purchases')
 
 class ReportView(LoginRequiredMixin, TemplateView):
-    template_name = "report.html"
+    template_name = "inventory/report.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["nbar"] = "report"
+        return context
 
 class RecipeRequirementCreateView(LoginRequiredMixin, CreateView):
     model = RecipeRequirement
     template_name = "inventory/reciperequirement_create.html"
+
+def log_out(request):
+    ''' Logout function '''
+    logout(request)
+    return redirect("/")
